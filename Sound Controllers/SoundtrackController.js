7da766b7bb9d11d5772699b168e7a34e40c8df04
Rolandas Barysas <rolandas@devlab.lt>
2012-11-06 15:21:09 +0200

    Initial commit

---
+++ Sound Controllers/SoundtrackController.js
@@ -0,0 +1,78 @@
+#pragma strict
+#pragma downcast
+
+// Soundtrack list
+var SoundtrackList : AudioClip[];
+
+// Booleans
+var FadeOutBetweenTracks : boolean = true;
+var FadeInNewTrack : boolean = true;
+
+// Speeds
+var FadeOutSpeed : float = 1.0;
+var FadeInSpeed : float = 1.0;
+
+// Delay between tracks
+var TransitionDelay : float = 1.0;
+
+// Additional (hidden)
+@HideInInspector var fadeOutBool : boolean = false;
+@HideInInspector var fadeInBool : boolean = false;
+@HideInInspector var fadeout_speed_fix : float = 1.0;
+@HideInInspector var fadein_speed_fix : float = 1.0;
+@HideInInspector var track_id : int;
+
+function Start() {
+	fadeout_speed_fix = 1 / FadeOutSpeed;
+	fadein_speed_fix = 1 / FadeInSpeed;
+}
+
+function playTrack(id) {
+	track_id = id;
+	if(audio.isPlaying && FadeOutBetweenTracks) {
+		fadeOutBool = true;
+	} else {
+		if(FadeInNewTrack) {
+			audio.volume = 0;
+			audio.clip = SoundtrackList[track_id];
+			audio.Play();
+			fadeIn();
+		} else {
+			audio.volume = 1;
+			audio.clip = SoundtrackList[track_id];
+			audio.Play();
+		}
+	}
+}
+
+function fadeOut() {
+	if(!fadeOutBool) {
+		audio.Stop();
+		yield WaitForSeconds(TransitionDelay);
+		playTrack(track_id);
+	}
+}
+
+function fadeIn() {
+	//playTrack(track_id);
+	fadeInBool = true;
+}
+
+function Update() {
+	if(fadeOutBool) {
+		if(audio.volume != 0) {
+			audio.volume -= 1 * Time.deltaTime * fadeout_speed_fix;
+		} else {
+			fadeOutBool = false;
+			fadeOut();
+		}
+	}
+
+	if(fadeInBool) {
+		if(audio.volume != 1) {
+			audio.volume += 1 * Time.deltaTime * fadein_speed_fix;
+		} else {
+			fadeInBool = false;
+		}
+	}
+}
